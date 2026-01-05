c045c5785e75b1387f73fe3ca281d9b033aa0b3e
Giampiero Salvi <giampisalvi@gmail.com>
2026-01-05 10:42:12 +0100

    tests with autocorrelation

--- python/detect_thermometer_beep.py
+++ python/detect_thermometer_beep.py
@@ -16,3 +16,55 @@
 # # Detect thermometer beep
 
 # %%
+from scipy.io import wavfile
+from scipy.signal import spectrogram
+from matplotlib import pyplot as plt
+import numpy as np
+from python_speech_features import sigproc
+
+# %%
+# frequency of termometer signal
+freq = 6080
+nfft = 2**12
+
+samplerate, data = wavfile.read('../data/thermometer00.wav')
+fft_bin = int(float(freq)*float(nfft)/float(samplerate))
+
+# use one of the two channels
+frames = sigproc.framesig(data[:, 0], nfft, nfft//2, )
+
+
+# %%
+autocorr = np.zeros((frames.shape[0], frames.shape[1]*2-1))
+for t in range(frames.shape[0]):
+    autocorr[t, :] = np.correlate(frames[t], frames[t], mode='full')
+
+# %%
+plt.imshow(np.log(autocorr))
+
+# %%
+print(f"fft bin = {fft_bin}")
+
+print(f"number of channels = {data.shape[1]}")
+length = data.shape[0] / samplerate
+print(f"length = {length}s")
+print('using channel 0')
+
+spgrm = spectrogram(data[:,0], fs=samplerate, nfft=nfft, nperseg=nfft, noverlap=nfft//2, scaling='spectrum')
+
+print(f"shape of spectrogram = {spgrm[2].shape}")
+
+spgrm_neg = spgrm[2]
+spgrm_neg[(fft_bin-2):(fft_bin+2),:] = 0
+detect = np.zeros((2, spgrm_neg.shape[1]))
+detect[0, :] = spgrm_neg.sum(axis=0)
+detect[1, :] = spgrm[2][(fft_bin-2):(fft_bin+2)].sum(axis=0)
+threshold = 0.1
+scaling = detect.sum(axis=0)
+scaling[scaling<threshold] = threshold
+detect = detect/scaling
+
+plt.plot(detect[1,:])
+
+
+# %%
